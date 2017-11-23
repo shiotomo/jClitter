827efbb1eae8b25fb626ed39113c063b9a8af1e4
shiotomo <t.shiota16@gmail.com>
2017-11-23 17:44:41 +0000

    Fix update_token

--- update_token.rb
+++ update_token.rb
@@ -1,3 +1,5 @@
+require "json"
+
 class UpdateToken
   def insert
     token = {}
@@ -15,16 +17,8 @@ class UpdateToken
   end
 
   def update(token)
-    if !File.exists?("json")
-      Dir.mkdir('json', 0755)
-    end
     File.open("json/token.json", "w") do |f|
-      f.puts "{"
-      f.puts "  \"consumer_key\"        : \"" + token["consumer_key"] + "\","
-      f.puts "  \"consumer_secret\"     : \"" + token["consumer_secret"] + "\","
-      f.puts "  \"access_token\"        : \"" + token["access_token"] + "\","
-      f.puts "  \"access_token_secret\" : \"" + token["access_token_secret"] + "\""
-      f.puts "}"
+      JSON.dump(token, f)
     end
   end
 end
